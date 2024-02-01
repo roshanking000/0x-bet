13d700a30a3f0fd6172ebdf99e5195684e002a91
cryptoroshan <royalguy1031@gmail.com>
2024-02-01 09:19:13 +0000

    complete all launchpad page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes, Navigate } from 'react-router-dom'
 
 import BaseLayout from "./layout/BaseLayout";
 import DashboardPage from './pages/dashboard';
+import LaunchpadPage from './pages/launchpad';
 
 function App() {
   return (
     <Routes>
       <Route element={<BaseLayout />}>
         <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/launchpad' element={<LaunchpadPage />} />
       </Route>
       <Route path="*" element={<Navigate to="/dashboard" />} />
     </Routes>
